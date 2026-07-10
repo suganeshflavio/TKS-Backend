e088eafcba33fb16f0f25ce6b9998d2fca8a974e
suganesh-s <suganeshwaran.selvaraj@agamtech.com>
2026-07-10 17:09:40 +0530

    build error resolve

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import dotenv from "dotenv";
 import app from "./app";
-import "../src/type/auth.types";
+// import "../src/type/auth.types";
 import { env } from "./config/env";
 
 dotenv.config();
-console.log("deno", process.env.JWT_SECRET);
 const PORT = env.PORT || 5000;
 
 app.listen(PORT, () => {
